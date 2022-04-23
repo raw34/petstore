2e35313ad0bc403fb4aa140d6fccc549aad5f0f9
raw34 <raw34@sina.com>
2022-04-23 22:28:55 +0800

    update

--- internal/server/http.go
+++ internal/server/http.go
@@ -1,12 +1,12 @@
 package server
 
 import (
-	v1 "github.com/raw34/petstore/api/helloworld/v1"
-	"github.com/raw34/petstore/internal/conf"
-	"github.com/raw34/petstore/internal/service"
 	"github.com/go-kratos/kratos/v2/log"
 	"github.com/go-kratos/kratos/v2/middleware/recovery"
 	"github.com/go-kratos/kratos/v2/transport/http"
+	v1 "github.com/raw34/petstore/api/helloworld/v1"
+	"github.com/raw34/petstore/internal/conf"
+	"github.com/raw34/petstore/internal/service"
 )
 
 // NewHTTPServer new a HTTP server.
