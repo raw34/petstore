2b743ce52fd84a6e825669f3f29748fcfed38558
raw34 <raw0034@gmail.com>
2022-05-02 12:11:49 +0800

    bugfix

---
+++ internal/service/greeter.go
@@ -0,0 +1,25 @@
+package service
+
+import (
+	"context"
+	v1 "github.com/raw34/petstore/api/greeter/v1"
+	"github.com/raw34/petstore/internal/biz"
+)
+
+type GreeterService struct {
+	v1.UnimplementedGreeterServer
+
+	uc *biz.GreeterUsecase
+}
+
+func NewGreeterService(uc *biz.GreeterUsecase) *GreeterService {
+	return &GreeterService{uc: uc}
+}
+
+func (s *GreeterService) GreeterSayHello(ctx context.Context, req *v1.GreeterSayHelloRequest) (*v1.GreeterV1HelloReply, error) {
+	g, err := s.uc.CreateGreeter(ctx, &biz.Greeter{Hello: req.Name})
+	if err != nil {
+		return nil, err
+	}
+	return &v1.GreeterV1HelloReply{Message: "Hello " + g.Hello}, nil
+}
